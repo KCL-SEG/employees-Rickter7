f222ffc8a03c0a00a1563fa27113b6c950e56310
Amoshe, Richard (k21057746) <richard.amoshe@kcl.ac.uk>
2022-10-04 12:53:42 +0100

    Added hours and rates

--- employee.py
+++ employee.py
@@ -49,16 +49,16 @@ class ContractEmployee(Employee):
 billie = SalaryEmployee('Billie', 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = ContractEmployee('Charlie')
+charlie = ContractEmployee('Charlie', 25, 100)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = SalaryEmployee('Renee', 3000)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = ContractEmployee('Jan')
+jan = ContractEmployee('Jan', 25, 150)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = SalaryEmployee('Robbie')
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = ContractEmployee('Ariel')
+ariel = ContractEmployee('Ariel', 30, 120)
