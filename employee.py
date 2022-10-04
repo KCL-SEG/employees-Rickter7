e2a028454749ddfeeac858aae32245669500969a
Amoshe, Richard (k21057746) <richard.amoshe@kcl.ac.uk>
2022-10-04 12:40:23 +0100

    Update employee.py

--- employee.py
+++ employee.py
@@ -3,6 +3,7 @@
 
 class Employee:
     totalPay = 0
+    string = ""
 
     def __init__(self, name):
         self.name = name
@@ -16,11 +17,17 @@ class Employee:
     def __str__(self):
         return self.name
 
+    def editString(string):
+        Employee.string = string
+
 class SalaryEmployee(Employee):
     def __init__(self, name, salary):
         super().__init__(self,name)
         self.salary = salary
         self.addSalary()
+
+    def getSalary(self):
+        return self.salary
 
     def addSalary(self):
         Employee.addToPay(self.salary)
@@ -32,11 +39,14 @@ class ContractEmployee(Employee):
         self.rate = rate
         self.hours = hours
 
+    def __str__(self):
+        return (self.name + " works on a monthly salary of " + self.getSalary() +". Their total pay is " + self.get_pay() + ".")
+
     def calculateSalary(self):
         salary = self.rate * self.hours
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = SalaryEmployee('Billie', 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie')
